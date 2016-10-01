9e6a456464fcd7966bf011768fc550eaf5be8062
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2016-10-01 22:25:02 +0200

    Default connection options.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
-console.log('Hi!')
+// import * as nsq from 'nsqjs'
+import * as Seneca from 'seneca'
+type Seneca = typeof Seneca
+
+export interface NsqOptions {
+  lookupdHost?: string,
+  lookupdPort?: number,
+  nsqdHost?: string,
+  nsqdPort?: number
+}
+
+let nsqOptionsDefaults = {
+  lookupdHost: '127.0.0.1',
+  lookupdPort: 4161,
+  nsqdHost: '127.0.0.1',
+  nsqdPort: 4150
+}
+
+function fillNsqOptions (options: NsqOptions) : NsqOptions {
+  let result = {} as NsqOptions
+  for (let k of Object.keys(nsqOptionsDefaults)) {
+    if (typeof options[k] === typeof nsqOptionsDefaults[k]) {
+      result[k] = options[k]
+    } else {
+      result[k] = nsqOptionsDefaults[k]
+    }
+  }
+  return result
+}
+
+export interface NsqForwarder {
+  (this: Seneca) : Seneca
+}
+
+let internal = {
+  readonly fillNsqOptions
+}
+export {
+  internal as _
+}
--- src/test/test.ts
+++ src/test/test.ts
@@ -3,8 +3,15 @@ import * as chaiAsPromised from 'chai-as-promised'
 chai.use(chaiAsPromised)
 let expect = chai.expect
 
-describe('System', () => {
-  it('Works', () => {
-    expect(2 + 2).to.equal(4)
+import * as main from '../main'
+let {fillNsqOptions} = main._
+
+describe('NSQ options', () => {
+  it('Fills defaults', () => {
+    let defaults = fillNsqOptions({})
+    expect(defaults.lookupdHost).to.equal('127.0.0.1')
+    expect(defaults.lookupdPort).to.equal(4161)
+    expect(defaults.nsqdHost).to.equal('127.0.0.1')
+    expect(defaults.nsqdPort).to.equal(4150)
   })
 })
